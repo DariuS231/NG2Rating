5f5739b7d545aaf21f11bcdbfb7423520c5bb917
DariuS231 <dario17231@gmail.com>
2016-05-09 17:38:40 +0100

    Changed the input name logic

--- app/shared/rating/rating.component.ts
+++ app/shared/rating/rating.component.ts
@@ -10,6 +10,10 @@ export class RatingComponent {
     @Input() itemId: number;
     @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
 
+    inpustName:string;
+    ngOnInit() {
+      this.inpustName = this.itemId + '_rating';
+    }
     onClick(rating: number): void {
         this.rating = rating;
         this.ratingClick.emit({
